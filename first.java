ada066e345af9ea5801f76bd82af01f26fccf8a1
maheshakya <pmaheshakya4@gmail.com>
2012-01-19 11:53:42 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,33 @@
+class A{
+  int a;
+  int b;
+  int c;
+  A(int p, int q, int r){
+	  a=p;
+	  b=q;
+	  c=r;
+  }
+}
+  
+  class B extends A{
+	  int d;
+	  B(int l, int m, int n, int o){
+  	super(l,m,n);
+  	d=o;
+  }
+  void Show(){
+  System.out.println("a = " + a);
+  System.out.println("b = " + b);
+  System.out.println("c = " + c);
+  System.out.println("d = " + d);
+  }
+
+  
+  }
+public class Main {
+	public static void main(String args[]){
+		  B b = new B(4,3,8,7);
+		  b.Show();
+		  }
+}
+
